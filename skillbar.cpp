a971f3a654bcb7d63cf8b64f96e8cbe66ca1e64e
mahnooraminullah <122717309+mahnooraminullah@users.noreply.github.com>
2023-11-14 21:13:39 +0500

    skillbar.cpp

---
+++ skillbar.cpp
@@ -0,0 +1,18 @@
+#include "skillbar.hpp"
+
+SkillBar::SkillBar(int s) : value(s) {}
+
+void SkillBar::displaySkillBar() const {
+    std::cout << "Skill Bar: " << value << "\n";
+}
+
+void SkillBar::increaseSkill(int amount) {
+    value += amount;
+}
+
+void SkillBar::decreaseSkill(int amount) {
+    value -= amount;
+    if (value < 0) {
+        value = 0;
+    }
+}
