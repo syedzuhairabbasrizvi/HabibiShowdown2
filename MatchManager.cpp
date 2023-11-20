d054656f31c45d33a83cb7bc099694f03d7f6d5c
Shaheer Abbas <114511098+syedshaheerabbasrizvi@users.noreply.github.com>
2023-11-20 20:49:55 +0500

    Update MatchManager.cpp

--- MatchManager.cpp
+++ MatchManager.cpp
@@ -8,3 +8,13 @@ void MatchManager::printGameState() const
 {
     std::cout << "Current Game State: " << gameState << std::endl;
 }
+
+string MatchManager::getGameState() const
+{
+    return gameState;
+}
+
+void MatchManager::setGameState(string newState)  
+{
+    gameState = newState;
+}
