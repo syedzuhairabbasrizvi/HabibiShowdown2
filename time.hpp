e67e1524b7d4054fb9c57d8bd84ef7e28fb640da
zuhair <122153573+syedzuhairabbasrizvi@users.noreply.github.com>
2023-11-23 00:21:44 +0500

    Update time.hpp
    Made the timer, I didn't make one for update so keep that in mind (or maybe I did? Doubt it)

--- time.hpp
+++ time.hpp
@@ -1,14 +1,26 @@
+// Timer.hpp
 #pragma once
 
-#include <iostream>
+#include <SDL_ttf.h>
+#include <SDL.h>
+#include <string>
 
-class Time {
-private:
-    int hours;
-    int minutes;
-
+class Timer {
 public:
-    Time(int h, int m);
+    Timer(SDL_Renderer* renderer, int screenWidth, int fontSize);   //construct timer using sdl, screen width, font size.
+    ~Timer();       //bye bye timer no one will miss you
 
-    void displayTime() const;
+    void start();   //start the timer
+    void update();  //idk i prolly will delete this one, ignore for now
+    void render();  //render the tiemr
+
+private:
+    Uint32 startTime;   //the actual ticking
+    SDL_Texture* timerTexture;  //shaheer says i shouldnt worry about this one
+    SDL_Renderer* renderer;     //important thing i guess?
+    TTF_Font* font;             //the font to use
+    int screenWidth;
+    
+    void loadFontTexture(const std::string& text);  //oh god
 };
+
