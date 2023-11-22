e93354ab0ed4e9b5ef47223bfc91ef191c5217e8
zuhair <122153573+syedzuhairabbasrizvi@users.noreply.github.com>
2023-11-23 00:17:44 +0500

    Update SDLHelper.hpp
    Added TrueType header

--- SDLHelper.hpp
+++ SDLHelper.hpp
@@ -2,6 +2,7 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 
 bool initSDL(SDL_Window*& window, SDL_Renderer*& renderer, const char* title, int width, int height);
 void closeSDL(SDL_Window*& window, SDL_Renderer*& renderer);
