44e7d1800b91b365ff54ca7a9e9b73a68fc8b68f
Shaheer Abbas <114511098+syedshaheerabbasrizvi@users.noreply.github.com>
2023-11-27 00:41:57 +0500

    Update health.hpp

--- health.hpp
+++ health.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <iostream>
+#include <SDL.h>
+
+using namespace std;
 
 class Health {
 private:
     int value;
+    int maxHealth;
 
 public:
     Health(int h);
-
+    void healthbar(SDL_Renderer* Renderer, int x, int y, int w, int h, SDL_Color FGColor, SDL_Color BGColor);
     void displayHealth() const;
     void takeDamage(int damage);
+    int getValue()
+    {
+        return value;
+    }
 };
