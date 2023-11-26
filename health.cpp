422caa5e01611c5e1aba9ee3b976530c39f1f993
Shaheer Abbas <114511098+syedshaheerabbasrizvi@users.noreply.github.com>
2023-11-27 00:41:44 +0500

    Update health.cpp

--- health.cpp
+++ health.cpp
@@ -1,9 +1,24 @@
 #include "health.hpp"
+#include <iostream>
 
-Health::Health(int h) : value(h) {}
+Health::Health(int h) : value(h), maxHealth(200) {}
+
+void Health::healthbar(SDL_Renderer* Renderer, int x, int y, int w, int h, SDL_Color FGColor, SDL_Color BGColor) {
+    float Percent = 1 - float(value)/float(maxHealth);
+    SDL_Color old;
+    SDL_GetRenderDrawColor(Renderer, &old.r, &old.g, &old.b, &old.a);
+    SDL_Rect bgrect = { x, y, w, h };
+    SDL_SetRenderDrawColor(Renderer, BGColor.r, BGColor.g, BGColor.b, BGColor.a);
+    SDL_RenderFillRect(Renderer, &bgrect);
+    SDL_SetRenderDrawColor(Renderer, FGColor.r, FGColor.g, FGColor.b, FGColor.a);
+    int pw = (int)((float)w * Percent);
+    int px = x + (w - pw);
+    SDL_Rect fgrect = { px, y, pw, h };
+    SDL_RenderFillRect(Renderer, &fgrect);
+    SDL_SetRenderDrawColor(Renderer, old.r, old.g, old.b, old.a);
+}
 
 void Health::displayHealth() const {
-    std::cout << "Health: " << value << "\n";
 }
 
 void Health::takeDamage(int damage) {
@@ -12,9 +27,8 @@ void Health::takeDamage(int damage) {
         value = 0;
         std::cout << "Try Again! You lost.";
     }
-  if (value < 10) {
-    std::cout << "WARNING:LOW HEALTH!";
-  }
-  //if other player's value = 0; you won
-  //
+    if (value < 10) {
+    }
+    // If other player's value = 0; you won
 }
+
